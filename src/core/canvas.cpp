4711cd9d8b66ed1d006e40d9bfca354f967f5ae0
Hope2333 <Hope2333@users.noreply.github.com>
2026-04-03 17:25:15 +0800

    Fix SkImage::makeShader for Skia m100: add SkSamplingOptions parameter.

--- src/core/canvas.cpp
+++ src/core/canvas.cpp
@@ -189,7 +189,7 @@ void drawTransparencyMesh(SkCanvas * const canvas,
     const float dim = eSizesUI::widget*0.5f / (scale > 1.f ? 1.f : scale);
     matr.setScale(dim, dim);
     auto image = SkImage::MakeFromBitmap(bitmap);
-    auto shader = image ? image->makeShader(SkTileMode::kRepeat, SkTileMode::kRepeat) : nullptr;
+    auto shader = image ? image->makeShader(SkSamplingOptions{SkFilterMode::kLinear}, SkTileMode::kRepeat, SkTileMode::kRepeat) : nullptr;
     if(shader && !matr.isIdentity()) {
         shader = shader->makeWithLocalMatrix(matr);
     }
